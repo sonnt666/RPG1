eaf4dad38b2b9c29376192197cdbdead5768083c
levantuyen05 <167695196+levantuyen05@users.noreply.github.com>
2024-08-13 20:15:46 +0700

    update login

--- Assets/Scripts/LoginRegister/DangKyTaiKhoan.cs
+++ Assets/Scripts/LoginRegister/DangKyTaiKhoan.cs
@@ -1,29 +1,35 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
+using static UnityEditor.ShaderData;
 
-public class DangKyTaiKhoan : MonoBehaviour
+public class dangkytaikhoan : MonoBehaviour
 {
-    public TMP_InputField user;
-    public TMP_InputField passwd;
+    public TMP_InputField username;
+    public TMP_InputField password;
     public TextMeshProUGUI thongbao;
 
-    public void DangKyButton()
+
+    public Button chuyendn;
+    public GameObject dangky;
+
+    public void dangkybutton()
     {
         StartCoroutine(DangKy());
-    }
 
+
+    }
     private IEnumerator DangKy()
     {
-        WWWForm form = new WWWForm();
-        form.AddField("user", user.text);
-        form.AddField("passwd", passwd.text);
+        WWWForm Form = new WWWForm();
+        Form.AddField("user", username.text);
+        Form.AddField("passwd", password.text);
 
-        UnityWebRequest www = UnityWebRequest.Post("https://fpl.expvn.com/dangky.php", form);
-
+        UnityWebRequest www = UnityWebRequest.Post("https://fpl.expvn.com/dangky.php", Form);
         yield return www.SendWebRequest();
-
         if (!www.isDone)
         {
             thongbao.text = "Kết nối không thành công";
@@ -31,14 +37,21 @@ public class DangKyTaiKhoan : MonoBehaviour
         else
         {
             string get = www.downloadHandler.text;
-
             switch (get)
             {
-                case "exist": thongbao.text = "Tài khoản đã tồn tại"; break;
-                case "OK": thongbao.text = "Đăng ký thành công"; break;
-                case "ERROR": thongbao.text = "Đăng ký không thành công"; break;
-                default: thongbao.text = "Không kết nối được server"; break;
+                case "exist": thongbao.text = " tài khoản đã tồn tại"; break;
+                case "OK": thongbao.text = "Đăng kí thành công"; break;
+                case "ERROR": thongbao.text = "Đăng kí không thành công"; break;
+                default: thongbao.text = "không kết nối được với sever"; break;
             }
         }
+    }
+    public void SwitchForm()
+    {
+        dangky.SetActive(!dangky.activeSelf);
+    }
+    void Start()
+    {
+        chuyendn.onClick.AddListener(SwitchForm);
     }
 }
--- Assets/Scripts/LoginRegister/DangNhapTaiKhoan.cs
+++ Assets/Scripts/LoginRegister/DangNhapTaiKhoan.cs
@@ -4,12 +4,17 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class DangNhapTaiKhoan : MonoBehaviour
 {
-    public TMP_InputField user;
-    public TMP_InputField passwd;
+    public TMP_InputField username;
+    public TMP_InputField password;
     public TextMeshProUGUI thongbao;
-
+    //chuyển đăng nhập với đăng kí
+    public Button chuyendk;
+    public GameObject dangnhap;
 
 
 
@@ -17,43 +22,57 @@ public class DangNhapTaiKhoan : MonoBehaviour
     {
         StartCoroutine(DangNhap());
     }
-
     IEnumerator DangNhap()
     {
         WWWForm form = new WWWForm();
-        form.AddField("user", user.text);
-        form.AddField("passwd", passwd.text);
+        form.AddField("user", username.text);
+        form.AddField("passwd", password.text);
 
         UnityWebRequest www = UnityWebRequest.Post("https://fpl.expvn.com/dangnhap.php", form);
-        yield return www.SendWebRequest(); //chờ phản hồi từ máy chủ
-
+        yield return www.SendWebRequest();
         if (!www.isDone)
         {
             thongbao.text = "Kết nối không thành công";
+
         }
         else
         {
             string get = www.downloadHandler.text;
-
-            if(get == "empty")
+            if (get == "empty")
             {
-                thongbao.text = "Các trường không được để trống";
-            }else if(get == "" || get == null)
+                thongbao.text = "Các thông tin không được để trống";
+            }
+            else if (get == "" || get == null)
             {
-                thongbao.text = "Tài khoản hoặc mật khẩu không đúng";
-            }else if (get.Contains("Lỗi"))
+                thongbao.text = "Tài khoản hoặc mật khẩu không chính xác";
+            }
+            else if (get.Contains("Lỗi"))
             {
-                thongbao.text = "không kết nối được server";
+                thongbao.text = "Không kết nối được tới server";
             }
             else
             {
                 thongbao.text = "Đăng nhập thành công";
                 PlayerPrefs.SetString("token", get);
+                Debug.Log(get);
+                SceneManager.LoadScene(1);
             }
-
-            string token = PlayerPrefs.GetString("token");
-            Debug.Log(token);
         }
     }
-    
+    void Start()
+    {
+        chuyendk.onClick.AddListener(SwitchForm);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SwitchForm()
+    {
+        dangnhap.SetActive(!dangnhap.activeSelf);
+    }
 }
+    
+
